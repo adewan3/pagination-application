d5563cafce1d658ce1675902f357a458c64707ae
adewan3 <adewan3@asu.edu>
2023-08-15 09:49:09 -0700

    Version 1 of Application for pagination

---
+++ App.js
@@ -0,0 +1,32 @@
+import React from 'react'
+// import DataForm from './DataForm/components/DataForm'
+// import AutoSearch from './AutoSearch/components/AutoSearch'
+
+// import VoteApp from './VoteApp';
+// import Hoocks from './Hoocks/Hoocks';
+// import Memo from './Hoocks/Memo';
+import Pagination from './Pagination';
+// import FolderApp from './FolderProj/FolderApp'
+
+
+
+
+const App = () => {
+  return (
+    <div>
+        
+        {/* <FolderApp/> */}
+        {/* <DataForm/> */}
+        {/* <AutoSearch/> */}
+        
+        {/* <VoteApp/> */}
+        <Pagination/>
+
+        {/* <Memo/> */}
+        
+      
+    </div>
+  )
+}
+
+export default App
